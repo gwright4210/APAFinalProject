0a27f3957db756a0262db9af58139aaaa0e14f9e
Grady Wright <gradywright25@amdg.rockhursths.edu>
2023-12-04 11:45:23 -0600

    firstdraft

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main{
+
+}
+
+class Grid{
+    //initialize array
+    private char[][] grid = new char[6][7];
+
+    public Grid(){
+        for (int row = 0; row < grid.length; row++){
+            for (int col = 0; col < grid[0].length; col++){
+                grid[row][col] = ' ';
+            }
+        }
+    }
+    public void setCell(int row, int col, char value){
+        grid[row][col] = value;
+    }
+
+    public char[][] getGrid(){
+        return grid;
+    }
+}
+
+class GridView{
+    public void displayGrid(char[][] grid){
+        for(char[] row : grid){
+            for(char cell : row){
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class GridController{
+    private Grid model;
+    private GridView view;
+
+    public GridController(Grid model, GridView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void updatedView() {
+        view.displayGrid(model.getGrid());
+    }
+
+     public void setCell(int row, int col, char value){
+       model.setCell(row, col, value);
+    }
+}
